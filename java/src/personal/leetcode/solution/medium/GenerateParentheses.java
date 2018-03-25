b0ebb87097c80c39f5a4a6d8060f71a305400d49
tsungta <david98_76@yahoo.com.tw>
2018-03-25 23:37:28 +0800

    update SingleNumber and ImplementStrStr solution.

---
+++ java/src/personal/leetcode/solution/medium/GenerateParentheses.java
@@ -0,0 +1,39 @@
+package personal.leetcode.solution.medium;
+
+import java.util.ArrayList;
+
+/*
+ * link: https://leetcode.com/problems/generate-parentheses/description/
+ */
+public class GenerateParentheses {
+	private static final char LEFT_PARENTHESIS = '(';
+	private static final char RIGHT_PARENTHESIS = ')';
+	
+    public List<String> generateParenthesis(int n) {
+        ArrayList<String> res = ArrayList<>();
+        
+        StringBuilder baseParentheses = new StringBuilder();
+        for(int i = 0; i < n - 1; i++) {
+        	baseParentheses.insert(0, LEFT_PARENTHESIS);
+        	baseParentheses.insert(baseParentheses.length(), RIGHT_PARENTHESIS);
+        }
+        
+        res.add(baseParentheses.toString());
+        
+        for(int i = 1; i < baseParentheses.length() - 1; i++) {
+        	if(baseParentheses.charAt(i) == baseParentheses.charAt(i + 1)) {
+        		continue;
+        	}
+        	swap(baseParentheses, i, i + 1);
+        	res.add(baseParentheses.toString());
+        }
+        
+        return res;
+    }
+    
+    private static void swap(StringBuilder sb, int index1, int index2) {
+    	char c = sb.charAt(index2);
+    	sb.setCharAt(index2, sb.charAt(index1));
+    	sb.setCharAt(index1, c);
+    }
+}
