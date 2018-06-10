bdfefdbc287d4235e9d2273df5b65512c54c44cd
tsungta <david98_76@yahoo.com.tw>
2018-06-10 22:28:48 +0800

    Update answer of Partition List.

---
+++ java/src/personal/leetcode/solution/medium/PartitionList.java
@@ -0,0 +1,44 @@
+package personal.leetcode.solution.medium;
+
+/**
+ * link: https://leetcode.com/problems/partition-list/description/
+ * 
+ * Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+ * You should preserve the original relative order of the nodes in each of the two partitions.
+ * 
+ * Example:
+ * 		Input: head = 1->4->3->2->5->2, x = 3
+ * 		Output: 1->2->2->4->3->5
+ */
+public class PartitionList {
+	public class ListNode {
+		int val;
+		ListNode next;
+		ListNode(int x) { val = x; }
+	}
+	
+	public ListNode partition(ListNode head, int x) {
+        ListNode lessHead = new ListNode(0);
+        ListNode largeHead = new ListNode(0);
+        ListNode lessTail = lessHead;
+        ListNode largeTail = largeHead;
+        
+        while(head != null) {
+            if(head.val >= x) {
+                largeTail.next = head;
+                largeTail = largeTail.next;
+            } else {
+                lessTail.next = head;
+                lessTail = lessTail.next;
+            }
+            head = head.next;
+            
+            lessTail.next = null;
+            largeTail.next = null;
+        }
+        
+        lessTail.next = largeHead.next;
+        
+        return lessHead.next;
+    }
+}
